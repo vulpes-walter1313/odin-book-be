2e8f6ea441c5d18d3fd6981536a46896e78e05ba
Walter Fritsche <wfritsche95@protonmail.com>
2025-02-13 09:55:54 -0600

    defined the req.user object

---
+++ requestuser.d.ts
@@ -0,0 +1,14 @@
+import * as express from "express";
+// check what shape a user object is being passed into passport on app.ts
+declare global {
+  namespace Express {
+    interface User {
+      id: string;
+      name: string;
+      username: string;
+      email: string;
+      profileImg: string | null;
+      bannedUntil: Date | null;
+    }
+  }
+}
