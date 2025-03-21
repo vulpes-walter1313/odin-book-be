c1d3b899e5fb8f50a1a4803e12da8f30f9b4937f
Walter Fritsche <wfritsche95@protonmail.com>
2025-03-21 11:01:25 -0500

    added users followers route and controller

--- routes/profiles.ts
+++ routes/profiles.ts
@@ -9,5 +9,6 @@ router.get("/:username", profileController.profile_GET);
 router.post("/:username/follow", profileController.profileFollow_POST);
 router.delete("/:username/follow", profileController.profileUnfollow_DELETE);
 router.get("/:username/following", profileController.profileFollowing_GET);
+router.get("/:username/followers", profileController.profileFollowers_GET);
 
 export default router;
